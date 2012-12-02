149f19b1eb8735ca99dcfba28d14a803e48b7e7f
Rudy Godoy <rudygodoyg@gmail.com>
2012-12-02 13:21:14 -0500

    Procesado de archivo de riesgo sanitario. Requisitos: Convertir el formato Excel a OpenOffice.org o LibreOffice

---
+++ util/ooo.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+#
+# Lee un archivo de datos en formato OOo/LibreOffice
+# se extrae los valores por cada celda y se procesa
+# generando un archivo .CSV (delimitado por comas)
+#
+# Requisitos: 
+# - Archivo se debe convertir a .ods 
+# - Nombre de archivo debe ser "todo.ods"
+#
+require 'rubygems'
+require 'roo'
+
+oo = Openoffice.new("todo.ods")
+oo.default_sheet = oo.sheets.first
+4.upto(oo.last_row) do |line|
+  codigo = oo.cell(line, 'A').to_s
+  codigo = codigo[0..codigo.index('.')-1]
+  if codigo.length < 6
+    tmp = "0"
+    codigo = tmp + codigo
+  end
+  cod_dpto = codigo[0, 2]
+  cod_prov = codigo[2, 2]
+
+  distrito = oo.cell(line, 'B')
+  distrito = distrito[distrito.index('Dis')..-1]
+  riesgo_cisterna = oo.cell(line, 'F')
+  riesgo_pozo = oo.cell(line, 'G')
+  riesgo_acequia = oo.cell(line, 'H')
+  riesgo_vecino = oo.cell(line, 'I')
+  riesgo_total = riesgo_cisterna + riesgo_pozo + riesgo_acequia + riesgo_vecino
+
+  if codigo
+    puts "#{codigo}, #{cod_dpto}, #{cod_prov}, #{distrito}, #{riesgo_cisterna}, #{riesgo_pozo}, #{riesgo_acequia}, #{riesgo_vecino},#{riesgo_total}"
+  end
+end
